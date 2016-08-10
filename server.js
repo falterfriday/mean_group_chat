16b248caf6715a532a6d2c0f81e39f63b18a84b8
Patrick Todd <patrick.a.todd@gmail.com>
2016-08-09 22:07:34 -0400

    it's complete

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
-// require express and path
 var express = require("express");
 var path = require("path");
-// create the express app
 var app = express();
-// static content
 app.use(express.static(path.join(__dirname, "./static")));
-// setting up ejs and our views folder
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
-// root route to render the index.ejs view
+
 app.get('/', function(req, res) {
  res.render("index");
 });
-// tell the express app to listen on port 8000
+
 var server = app.listen(8000, function() {
  console.log("listening on port 8000");
 });
+var messages = [];
 
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket) {
-	socket.on("button_clicked", function (data){
-		console.log('Someone clicked a button!  Reason: ' + data.reason);
-		socket.emit('server_response', {response: "sockets are the best!"});
-		});
-	console.log('WE ARE USING SOCKETS!');
-	console.log(socket.id);
+
+	// socket.on('new_user', function(data) {
+		// console.log(messages);
+	// });
+
+	socket.emit( 'message_data', messages );
+
+	socket.on('new_message', function(data){
+		console.log(data);
+		messages.push(data);
+		console.log(messages);
+		io.emit( 'single_message', messages );
+	});
 });
